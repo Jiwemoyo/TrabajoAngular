b26cce6edcadeea77f840f521c75c676bc15dc6e
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2023-05-02 15:10:29 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WebComponent } from './web/web.component';
+import { SharedComponent } from './shared/shared.component';
+import { PagesComponent } from './pages/pages.component';
+import { AuthComponent } from './auth/auth.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductComponent } from './pages/product/product.component';
+import { CategoryComponent } from './pages/category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebComponent,
+    SharedComponent,
+    PagesComponent,
+    AuthComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    SidebarComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProductComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
